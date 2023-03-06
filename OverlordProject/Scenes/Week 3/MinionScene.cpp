ec57eaeabaddb1fc0d1060f9dc4c30e35fcc59e8
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-03-06 15:10:39 +0100

    Lab03 Added Torus Demo
    Added Torus Demo level from lab 03

--- OverlordProject/Scenes/Week 3/MinionScene.cpp
+++ OverlordProject/Scenes/Week 3/MinionScene.cpp
@@ -2,8 +2,7 @@
 #include "stdafx.h"
 #include "MinionScene.h"
 
-#include "Prefabs/CubePrefab.h"
-#include "Prefabs/SpherePrefab.h"
+#include "Prefabs/TorusPrefab.h"
 
 
 MinionScene::MinionScene() :
@@ -17,11 +16,26 @@ void MinionScene::Initialize()
 	m_SceneContext.settings.enableOnGUI = true;
 
 	Logger::LogInfo(L"Welcome, humble Minion!");
+
+	// Torus big
+	m_pTorusBig = new TorusPrefab{ 10, 50, 1, 20, XMFLOAT4{ Colors::Red } };
+	AddChild(m_pTorusBig);
+
+	// Torus medium
+	m_pTorusMedium = new TorusPrefab{ 7, 50, 1, 20, XMFLOAT4{ Colors::Green } };
+	m_pTorusBig->AddChild(m_pTorusMedium);
+
+	// Torus small
+	m_pTorusSmall = new TorusPrefab{ 4, 50, 1, 20, XMFLOAT4{ Colors::Blue } };
+	m_pTorusMedium->AddChild(m_pTorusSmall);
 }
 
 void MinionScene::Update()
 {
-	//Optional
+	const float rotation{ 90.0f * m_SceneContext.pGameTime->GetTotal() };
+	m_pTorusBig->GetTransform()->Rotate(0.0f, rotation, 0.0f);
+	m_pTorusMedium->GetComponent<TransformComponent>()->Rotate(rotation, 0.0f, 0.0f);
+	m_pTorusSmall->GetTransform()->Rotate(0.0f, rotation, 0.0f);
 }
 
 void MinionScene::Draw()
